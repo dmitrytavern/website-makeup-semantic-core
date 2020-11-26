ec33a1c1d8c489e131ad28e56d64c5613af7e392
dogger-work <dogger.work@gmail.com>
2020-11-26 16:50:32 +0200

    0.5v - Added header animations and fancybox for prices

--- src/js/app.js
+++ src/js/app.js
@@ -4,4 +4,38 @@ import '../sass/app.sass'
 *   Common scripts for main page
 * */
 
+$(document).ready(function () {
+	document.body.classList.add('is-loaded')
+})
 
+
+//  Header
+
+const $header         = $('header')
+const $headerMenuBtn  = $('#header-menu-btn')
+const $menu           = $('.menu')
+
+function scrollPage() {
+	if (window.pageYOffset > 0) {
+		$header.addClass('is-scroll')
+	} else {
+		$header.removeClass('is-scroll')
+	}
+
+	setTimeout(function () {
+		// if (window.innerWidth < 992) $menu.css('padding-top', $header.outerHeight(true))
+	}, 300)
+}
+
+function toggleHeaderMenu() {
+	$headerMenuBtn.toggleClass('is-active')
+	$header.toggleClass('is-menu-open')
+	$menu.toggleClass('is-open')
+	document.body.classList.toggle('menu-opened')
+}
+
+$($headerMenuBtn).on('click', toggleHeaderMenu)
+
+$(window).on('resize', scrollPage)
+$(window).on('scroll', scrollPage)
+scrollPage()
