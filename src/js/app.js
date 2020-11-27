c7002f5d14a7453d0c5a5cffaeb23dcde2fd6029
dogger-work <dogger.work@gmail.com>
2020-11-27 19:49:58 +0200

    0.20v - Fixed bugs and question animation. Added spyscroll, anchor animation

--- src/js/app.js
+++ src/js/app.js
@@ -13,6 +13,18 @@ $(".btn-up").click(function() {
 	return false;
 });
 
+$("#header-anchors-navigation li a").on('click', function(event) {
+	if (this.hash !== "") {
+		event.preventDefault();
+
+		const hash = this.hash;
+
+		$('html, body').animate({
+			scrollTop: $(hash).offset().top - 150
+		}, 800)
+	}
+})
+
 
 //  Header
 
