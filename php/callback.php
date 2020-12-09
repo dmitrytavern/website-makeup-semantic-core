5422e4c221f4f3d6e8480398983e3b7210f2bc79
dogger-work <dogger.work@gmail.com>
2020-12-10 00:06:54 +0200

    2.6v - Update mail send

--- php/callback.php
+++ php/callback.php
@@ -1,76 +1,26 @@
 <?php
+require '../../library/Mailer.php';
 
-if (!empty($_POST))
-{
-    $ADDRESS_FROM = 'Semantic Core Site <nx-agency.com>';
-    $ADDRESS_TO = 'k.lyovushkin@gmail.com';
-    $MAIL_SUBJECT = 'Semantic Core Site Order';
+if (!empty($_POST)) {
 
-    // To send HTML mail, the Content-type header must be set
-    $headers[] = 'MIME-Version: 1.0';
-    $headers[] = 'X-Mailer: PHP/'.PHP_VERSION;
-    $headers[] = 'Content-type: text/html; charset=utf-8';
+    $params = [
+        ['title' => 'Адрес сайта или Тематика проекта:', 'answer' => strip_tags($_POST["site"])],
+        ['title' => 'Email или Мессенджер:', 'answer' => strip_tags($_POST["contact"])],
+        ['title' => 'Пример нужных запросов: ', 'answer' => strip_tags($_POST["examples"])],
+        ['title' => 'Сколько ТЗ нужно: ', 'answer' => strip_tags($_POST["count"])],
+        ['title' => 'Где вы узнали о наших услугах: ', 'answer' => strip_tags($_POST["fromKnow"])],
+    ];
 
-    // Additional headers
-    $headers[] = 'from: Semantic Core Site';
-    $headers[] = 'reply-to: '.$ADDRESS_FROM;
-
-
-    if (!empty($_POST['contact'])) {
-
-        $message = '
-        <html>
-            <head>
-              <title>Semantic Core Site Order</title>
-            </head>
-            <body style="color: #000000;">
-              <table style="padding: 30px 0">
-                <tr style="padding: 5px 0;">
-                  <td style="padding-right: 15px; font-size: 16px;"><b>Адрес сайта или Тематика проекта: </b></td><td>'. $_POST["site"] .'</td>
-                </tr>
-                <tr style="padding: 5px 0;">
-                  <td style="padding-right: 15px; font-size: 16px;"><b>Email или Мессенджер: </b></td><td>'. $_POST["contact"] .'</td>
-                </tr>
-                <tr style="padding: 5px 0;">
-                  <td style="padding-right: 15px; font-size: 16px;"><b>Пример нужных запросов: </b></td><td>'. $_POST["examples"] .'</td>
-                </tr>
-                <tr style="padding: 5px 0;">
-                  <td style="padding-right: 15px; font-size: 16px;"><b>Сколько ТЗ нужно: </b></td><td>'. $_POST["count"] .'</td>
-                </tr>
-                <tr style="padding: 5px 0;">
-                  <td style="padding-right: 15px; font-size: 16px;"><b>Где вы узнали о наших услугах: </b></td><td>'. $_POST["fromKnow"] .'</td>
-                </tr>
-              </table>
-            </body>
-            </html>
-        ';
-
-        $success = mail(
-            $ADDRESS_TO,
-            $MAIL_SUBJECT,
-            $message,
-            implode("\r\n", $headers)
-        );
-
-		if (!$success) {
-			echo 'Error';
-            echo error_get_last()['message'];
-        }
-
-        $arr = array('success' => $success);
-        echo json_encode($arr);
-    } else {
-        $arr = array('success' => false);
-        echo json_encode($arr);
+    $mail = new Library\Mailer($params);
+    $mail->Subject = 'Semantic Core Site Order';
+    $mail->addAddress('alex.pavlutskiy@gmail.com');
+    $success = $mail->send();
+    if (!$success) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        die();
     }
-
-//    echo $_POST['site'];
-//    echo $_POST['contact'];
-//    echo $_POST['examples'];
-//    echo $_POST['count'];
-//    echo $_POST['fromKnow'];
-}
-else
-{
+    echo json_encode(['success' => true]);
+    die();
+} else {
     header('Location: index.html');
 }
